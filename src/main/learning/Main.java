07210b60fd9a7dc576048b1e075ef61a623a5630
Boromir99 <denispetrea2000@gmail.com>
2023-09-01 10:21:42 +0300

    The greatest common divisor

--- src/main/learning/Main.java
+++ src/main/learning/Main.java
@@ -3,6 +3,22 @@ package learning;
 public class Main {
     public static void main(String[] args) {
 
+        int[] numbers = new int[]{8, 32, 64, 8};
 
+        int commonNumber = numbers[0];
+        for (int i = 1; i < numbers.length - 1; i++) {
+            commonNumber = gcd(commonNumber, numbers[i]);
+
+        }
+        System.out.println(commonNumber);
+    }
+
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
 }

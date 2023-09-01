1d426a91f147bcce45d000756ab7af901eef84b5
Boromir99 <denispetrea2000@gmail.com>
2023-09-01 11:55:23 +0300

    Grade Exam

--- src/main/learning/Main.java
+++ src/main/learning/Main.java
@@ -3,22 +3,30 @@ package learning;
 public class Main {
     public static void main(String[] args) {
 
-        int[] numbers = new int[]{8, 32, 64, 8};
 
-        int commonNumber = numbers[0];
-        for (int i = 1; i < numbers.length - 1; i++) {
-            commonNumber = gcd(commonNumber, numbers[i]);
+        char[][] answers = {
+                {'A', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'D', 'B', 'A', 'B', 'C', 'A', 'E', 'E', 'A', 'D'},
+                {'E', 'D', 'D', 'A', 'C', 'B', 'E', 'E', 'A', 'D'},
+                {'C', 'B', 'A', 'E', 'D', 'C', 'E', 'E', 'A', 'D'},
+                {'A', 'B', 'D', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'B', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'B', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'E', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'}};
 
+        char[] keys = {'D', 'B', 'D', 'C', 'C', 'D', 'A', 'E', 'A', 'D'};
+
+        for (int row = 0; row < answers.length; row++) {
+            int correctCount = 0;
+
+            for (int collum = 0; collum < answers[row].length; collum++) {
+                if (keys[collum] == answers[row][collum]) {
+                    correctCount++;
+                }
+            }
+            System.out.println("Student " + row + "'s correct count is " +
+                    correctCount);
         }
-        System.out.println(commonNumber);
-    }
-
-    public static int gcd(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
     }
 }
+

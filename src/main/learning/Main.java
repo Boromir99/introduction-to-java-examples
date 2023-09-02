b9015cc4650a012f84227d684ee3f353161f1a04
Boromir99 <denispetrea2000@gmail.com>
2023-09-02 12:35:13 +0300

    TV class

--- src/main/learning/Main.java
+++ src/main/learning/Main.java
@@ -3,45 +3,13 @@ package learning;
 public class Main {
     public static void main(String[] args) {
 
-        Circle circle1 = new Circle();
-        System.out.println("The area of the circle of radius "
-                + circle1.radius + " is " + circle1.getArea());
+        TV tv1 = new TV();
+        tv1.turnOn();
+        tv1.setChannel(30);
+        tv1.setVolume(3);
 
-        Circle circle2 = new Circle(25);
-        System.out.println("The area of the circle of radius "
-                + circle2.radius + " is " + circle2.getArea());
+        System.out.println("tv1's channel is " + tv1.channel
+                + " and volume level is " + tv1.volumeLevel);
 
-        Circle circle3 = new Circle(125);
-        System.out.println("The area of the circle of radius "
-                + circle3.radius + " is " + circle3.getArea());
-
-        circle2.radius = 100;
-        System.out.println("The area of the circle of radius "
-                + circle2.radius + " is " + circle2.getArea());
-
-    }
-}
-
-class Circle {
-    double radius;
-
-    Circle() {
-        radius = 1;
-    }
-
-    Circle(double newRadius) {
-        radius = newRadius;
-    }
-
-    double getArea() {
-        return radius * radius * Math.PI;
-    }
-
-    double getPerimeter() {
-        return 2 * radius * Math.PI;
-    }
-
-    void setRadius(double newRadius) {
-        radius = newRadius;
     }
 }

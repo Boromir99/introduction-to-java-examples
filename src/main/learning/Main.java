5e5d337303d67c41b6bbdb9ed445d2a75f2470d1
Boromir99 <denispetrea2000@gmail.com>
2023-09-13 21:03:27 +0300

    Sort students

--- src/main/learning/Main.java
+++ src/main/learning/Main.java
@@ -3,25 +3,27 @@ package learning;
 
 public class Main {
     public static void main(String[] args) {
-        int character = 0;
-        int number = 0;
 
-        String password = "Georgica78";
-
-        for (int i = 0; i < password.length(); i++) {
-            if (Character.isLetter(password.charAt(i))) {
-                character++;
-            } else if (Character.isDigit(password.charAt(i))) {
-                number++;
-            } else {
-                System.out.println("Invalid password");
-                break;
-            }
+        String[] stundents = {"Mihaela", "Geanina", "Robert"};
+        int[] grades = {7, 10, 9};
+        ordonare(stundents, grades);
+        for (int i = 0; i < stundents.length; i++) {
+            System.out.println(stundents[i]);
         }
-        if (character + number < 8)
-            System.out.println("To short");
-        else if (number < 2)
-            System.out.println("To few digits");
-        else System.out.println("Is Valid");
+    }
+
+    static String[] ordonare(String[] students, int[] grades) {
+        int minIndex = 0;
+
+        for (int i = 0; i < students.length; i++) {
+            for (int j = 1; j < students.length; j++) {
+                if (grades[j] > grades[j - 1])
+                    minIndex = j;
+            }
+            String temp = students[i];
+            students[i] = students[minIndex];
+            students[minIndex] = temp;
+        }
+        return students;
     }
 }

af70d3736c42fccd9a08cf786e8d4bac678e14f2
Boromir99 <denispetrea2000@gmail.com>
2023-09-14 13:21:53 +0300

    Game: hangman

--- src/main/learning/Main.java
+++ src/main/learning/Main.java
@@ -1,26 +1,65 @@
 package learning;
 
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-
-        System.out.print(order("erwoiajvaelbiru"));
+        String[] words = {"avracadavra", "animal", "baseball", "coffee"};
+        Random random = new Random();
+        spanzuratoarea(words[random.nextInt(words.length)]);
 
     }
-    static String order(String s){
-        char[] array = s.toCharArray();
 
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 1 + i; j < array.length; j++) {
-                if (array[j] < array[i]) {
-                    char temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
+    static void spanzuratoarea(String word) {
+        java.util.Scanner input = new Scanner(System.in);
+        char[] letters = word.toCharArray();
+        char[] duplicate = initialization(word);
+
+        System.out.println();
+        do {
+            int x = 0;
+            char c = input.next().charAt(0);
+            for (int i = 0; i < letters.length - 1; i++) {
+                if (c == letters[i]) {
+                    duplicate[i] = c;
+                    x++;
                 }
             }
+            if (x == 0)
+                System.out.println("Wrong letter, try again");
+            display(duplicate);
+            System.out.println();
+        } while (!Arrays.equals(duplicate, letters));
 
+        System.out.println("Congratulations! The word is " + word);
+    }
+
+    static void display(char[] word) {
+        for (char c : word) {
+            System.out.print(c);
         }
-        return new String(array);
+    }
+
+    static char[] initialization(String word) {
+        char[] letters = word.toCharArray();
+        char[] duplicate = new char[letters.length];
+
+        duplicate[0] = letters[0];
+        duplicate[letters.length - 1] = letters[letters.length - 1];
+        for (int i = 2; i < letters.length; i++) {
+            duplicate[i - 1] = '_';
+        }
+        for (int i = 1; i < letters.length; i++) {
+            if (letters[0] == letters[i])
+                duplicate[i] = letters[0];
+            if (letters[letters.length - 1] == letters[i])
+                duplicate[i] = letters[letters.length - 1];
+        }
+        display(duplicate);
+        return duplicate;
     }
 }

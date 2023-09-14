562b294cef97077ceb2963ec07188a0c4fec8ba9
Boromir99 <denispetrea2000@gmail.com>
2023-09-14 17:30:29 +0300

    Adunarea si inmultirea matricilor

--- src/main/learning/Main.java
+++ src/main/learning/Main.java
@@ -1,65 +1,114 @@
 package learning;
 
-
-import java.util.Arrays;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        String[] words = {"avracadavra", "animal", "baseball", "coffee"};
-        Random random = new Random();
-        spanzuratoarea(words[random.nextInt(words.length)]);
+        int[][] matrice1 = initializareMatrice();
+        int[][] matrice2 = initializareMatrice();
+        afisareMatrice(matrice1);
+        afisareMatrice(matrice2);
+        adunareMatrici(matrice1, matrice2);
+        inmultireMatrici(matrice1, matrice2);
 
     }
 
-    static void spanzuratoarea(String word) {
+    static int[][] initializareMatrice() {
         java.util.Scanner input = new Scanner(System.in);
-        char[] letters = word.toCharArray();
-        char[] duplicate = initialization(word);
+        int ordin;
 
-        System.out.println();
+        //Determinam ordinul matricei ce urmeaza sa fie implementata
+        //Dorim sa fie minim ordin 2
         do {
-            int x = 0;
-            char c = input.next().charAt(0);
-            for (int i = 0; i < letters.length - 1; i++) {
-                if (c == letters[i]) {
-                    duplicate[i] = c;
-                    x++;
+            System.out.print("Scrieti de ce ordin este matricea: ");
+            ordin = input.nextInt();
+            if (ordin < 2)
+                System.out.println("Ordinul matricii trebuie sa fie minim 2");
+        } while (ordin < 2);
+        int[][] matrice = new int[ordin][ordin];
+
+        //Scriem elementele unei matrici
+        for (int i = 0; i < ordin; i++) {
+            for (int j = 0; j < ordin; j++) {
+                System.out.print("Definiti matricea [" + i + "]" + "[" + j + "]: ");
+                matrice[i][j] = input.nextInt();
+            }
+        }
+        return matrice;
+    }
+
+    static void afisareMatrice(int[][] matrice) {
+        for (int[] i : matrice) {
+            for (int j : i) {
+                System.out.print(j + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    static int[][] adunareMatrici(int[][] matrice1, int[][] matrice2) {
+
+        int[][] adunare = new int[matrice1.length][matrice1.length];
+        if (egal(matrice1, matrice2)) {
+
+            //Adunam matricile
+            for (int i = 0; i < matrice1.length; i++) {
+                for (int j = 0; j < matrice1.length; j++) {
+                    adunare[i][j] = matrice1[i][j] + matrice2[i][j];
                 }
             }
-            if (x == 0)
-                System.out.println("Wrong letter, try again");
-            display(duplicate);
-            System.out.println();
-        } while (!Arrays.equals(duplicate, letters));
-
-        System.out.println("Congratulations! The word is " + word);
+            //Afisam suma
+            System.out.println("Suma matricilor este: ");
+            for (int[] i : adunare) {
+                for (int j : i) {
+                    System.out.print(j + "\t");
+                }
+                System.out.println();
+            }
+            return adunare;
+        } else System.out.println("Matricile nu sunt de acelasi ordin");
+        return null;
     }
 
-    static void display(char[] word) {
-        for (char c : word) {
-            System.out.print(c);
+    static boolean egal(int[][] matrice1, int[][] matrice2) {
+
+        //Verificam daca randurile sunt egale in lungime
+        if (matrice1.length != matrice2.length)
+            return false;
+
+        //Verificam daca coloanele sunt egale in lungime
+        for (int i = 0; i < matrice1.length; i++) {
+            if (matrice1[i].length != matrice2[i].length)
+                return false;
         }
+        return true;
     }
 
-    static char[] initialization(String word) {
-        char[] letters = word.toCharArray();
-        char[] duplicate = new char[letters.length];
+    static int[][] inmultireMatrici(int[][] matrice1, int[][] matrice2) {
+        if (egal(matrice1, matrice2)) {
+            int[][] produs = new int[matrice1.length][matrice1.length];
 
-        duplicate[0] = letters[0];
-        duplicate[letters.length - 1] = letters[letters.length - 1];
-        for (int i = 2; i < letters.length; i++) {
-            duplicate[i - 1] = '_';
+            //Inmultim matricile
+            for (int i = 0; i < matrice1.length; i++) {
+                for (int j = 0; j < matrice1.length; j++) {
+                    for (int k = 0; k < matrice1.length; k++) {
+                        produs[i][j] += matrice1[i][k] * matrice2[k][j];
+                    }
+                }
+            }
+
+            //Afisam produsul
+            System.out.println("Produsul matricilor este: ");
+            for (int[] i : produs) {
+                for (int j : i) {
+                    System.out.print(j + "\t");
+                }
+                System.out.println();
+            }
+            return produs;
         }
-        for (int i = 1; i < letters.length; i++) {
-            if (letters[0] == letters[i])
-                duplicate[i] = letters[0];
-            if (letters[letters.length - 1] == letters[i])
-                duplicate[i] = letters[letters.length - 1];
-        }
-        display(duplicate);
-        return duplicate;
+        return null;
     }
 }

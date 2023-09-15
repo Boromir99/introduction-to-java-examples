53a02f75c1fd2d8c6e82a09b05c905316e8bc44b
Boromir99 <denispetrea2000@gmail.com>
2023-09-15 17:40:42 +0300

    base 2 in base 10 and vice versa

--- src/main/learning/Main.java
+++ src/main/learning/Main.java
@@ -1,28 +1,44 @@
 package learning;
 
 
-
 public class Main {
     public static void main(String[] args) {
 
-        String str = "papagal";
-        String substr = "papa";
-        System.out.print(find(str, substr));
+        int baza10 = 602;
+        baza2(baza10);
+
+        int[] baza2 = {1, 0, 0, 1, 0, 1, 1, 0, 1, 0};
+        baza10(baza2);
     }
 
-    static boolean find(String str, String substr) {
+    static int[] baza2(int baza10) {
 
-        for (int i = 0; i < str.length() - substr.length(); i++) {
-            boolean match = true;
-            for (int j = 0; j < substr.length(); j++) {
-                if (str.charAt(i + j) != substr.charAt(j)) {
-                    match = false;
-                    break;
-                }
-            }
-            if (match)
-                return true;
+        int size = 100;
+        int[] baza2Inversa = new int[size];
+        int index = 0;
+
+        while (baza10 != 0) {
+            baza2Inversa[index] = baza10 % 2;
+            baza10 /= 2;
+            index++;
         }
-        return false;
+        int[] baza2 = new int[index];
+        for (int i = 0; i < baza2.length; i++) {
+            baza2[i] = baza2Inversa[index - 1];
+            index--;
+            System.out.print(baza2[i]);
+        }
+        System.out.println();
+        return baza2;
+    }
+
+    static void baza10(int[] baza2) {
+        int baza10 = 0;
+        for (int i = 0; i < baza2.length; i++) {
+            int doiLaPutere = (int) Math.pow(2, baza2.length - i - 1);
+            if (baza2[i] == 1) baza10 += doiLaPutere;
+
+        }
+        System.out.print(baza10);
     }
 }

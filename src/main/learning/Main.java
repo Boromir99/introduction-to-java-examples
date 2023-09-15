fa4fc563dc15041fb5b2a29ef5b99b3e2283af2b
Boromir99 <denispetrea2000@gmail.com>
2023-09-15 18:25:04 +0300

    Game: bean machine

--- src/main/learning/Main.java
+++ src/main/learning/Main.java
@@ -4,34 +4,28 @@ package learning;
 import java.util.Arrays;
 import java.util.Random;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
 
         int balls = 5;
         game(balls);
 
     }
 
-    static void game(int balls)
-    {
+    static void game(int balls) {
         Random random = new Random();
 
         int mazeLenght = 7;
         int[] slots = new int[mazeLenght + 1];
 
-        for (int i = 0; i < balls; i++)
-        {
+        for (int i = 0; i < balls; i++) {
             int baal = 0;
-            for (int j = 0; j < mazeLenght; j++)
-            {
+            for (int j = 0; j < mazeLenght; j++) {
                 int fall = random.nextInt(2);
-                if (fall == 1)
-                {
+                if (fall == 1) {
                     System.out.print("R");
                     baal++;
-                }else System.out.print("L");
+                } else System.out.print("L");
             }
             slots[baal] += 1;
             System.out.println();

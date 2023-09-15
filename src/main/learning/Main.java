357979ebc249c9b1930db1c74552e7521b9949a5
Boromir99 <denispetrea2000@gmail.com>
2023-09-15 19:37:49 +0300

    Largest row and column

--- src/main/learning/Main.java
+++ src/main/learning/Main.java
@@ -1,35 +1,55 @@
 package learning;
 
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
 
-        int balls = 5;
-        game(balls);
-
-    }
-
-    static void game(int balls) {
         Random random = new Random();
+        int[][] array = new int[4][4];
 
-        int mazeLenght = 7;
-        int[] slots = new int[mazeLenght + 1];
-
-        for (int i = 0; i < balls; i++) {
-            int baal = 0;
-            for (int j = 0; j < mazeLenght; j++) {
-                int fall = random.nextInt(2);
-                if (fall == 1) {
-                    System.out.print("R");
-                    baal++;
-                } else System.out.print("L");
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[array.length - 1].length; j++) {
+                int p = random.nextInt(2);
+                if (p == 1)
+                    array[i][j] = 1;
             }
-            slots[baal] += 1;
+        }
+        for (int[] i : array) {
+            for (int j : i)
+                System.out.print(j + " ");
             System.out.println();
         }
-        System.out.print(Arrays.toString(slots));
+
+        int largestRow = 0;
+        int curentRow = 1;
+        for (int i = 0; i < array[array.length - 1].length; i++) {
+            int count = 0;
+            for (int j = 0; j < array.length; j++) {
+                if (array[i][j] == 1)
+                    count++;
+            }
+            if (count > curentRow) {
+                curentRow = count;
+                largestRow = i + 1;
+            }
+        }
+        System.out.println("The richest row is: " + largestRow);
+
+        int largestCollum = 0;
+        int curentCoollum = 1;
+        for (int i = 0; i < array[array.length - 1].length; i++) {
+            int count = 0;
+            for (int j = 0; j < array.length; j++) {
+                if (array[j][i] == 1)
+                    count++;
+            }
+            if (count > largestCollum) {
+                curentCoollum = count;
+                largestCollum = i + 1;
+            }
+        }
+        System.out.println("The richest collum is: " + largestCollum);
     }
 }

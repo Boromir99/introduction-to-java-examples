925620396dedc54e32ac8620e6a0620989fa555d
Boromir99 <denispetrea2000@gmail.com>
2023-09-28 15:31:55 +0300

    Maximum consecutive increasingly ordered substring

--- src/main/learning/Main.java
+++ src/main/learning/Main.java
@@ -1,20 +1,40 @@
 package learning;
 
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
 
-        reverseDisplay("12345");
+        java.util.Scanner input = new Scanner(System.in);
+        String string = input.next();
+        subStr(string);
 
     }
 
-    public static void reverseDisplay(String value) {
-        if (value.length() == 0)
-            return;
-        else {
-            System.out.print(value.charAt(value.length() - 1));
-            reverseDisplay(value.substring(0, value.length() - 1));
+    public static void subStr(String string) {
+        int subStrindexStart = 0;
+        int subStrIndexEnd = 0;
+        int count = 0;
+
+        for (int i = 0; i < string.length(); i++) {
+            int j;
+            for (j = 0; i + j < string.length() - 1; j++) {
+                if (string.charAt(i + j) >= string.charAt(i + j + 1)) {
+                    break;
+                }
+            }
+            if (j > count) {
+                count = j;
+                subStrindexStart = i;
+                subStrIndexEnd = subStrindexStart + count;
+                i += j;
+            }
         }
 
+        for (int i = subStrindexStart; i <= subStrIndexEnd; i++) {
+            System.out.print(string.charAt(i));
+        }
     }
 }

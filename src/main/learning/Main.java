8a89fcc0ef93af974ffb59a3c9e928c6eceecbac
Boromir99 <denispetrea2000@gmail.com>
2023-10-05 20:02:38 +0300

    Maximum consecutive increasingly ordered substring, with a single for

--- src/main/learning/Main.java
+++ src/main/learning/Main.java
@@ -1,36 +1,46 @@
 package learning;
 
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+        java.util.Scanner input = new Scanner(System.in);
         String string = input.next();
-        String substring = input.next();
-        System.out.print(indexMatched(string, substring));
+        subStr(string);
 
     }
 
-    public static int indexMatched(String string, String substring) {
-        int index = 0;
+    public static void subStr(String string) {
+        int j1 = 0;
+        int j2 = 0;
         int count = 0;
+        int higher = 0;
 
-        if (substring.length() > string.length()) {
-            System.out.println("Substring is larger then the string");
-            return 0;
-        }
-        for (int i = 0; i < string.length(); i++) {
-            for (int j = 0; j < substring.length(); j++) {
-                if (substring.charAt(j) == string.charAt(i + j)) {
-                    count++;
-                } else break;
+        for (int i = 0; i < string.length() - 1; i++) {
+
+            if (string.charAt(i) >= string.charAt(i + 1)) {
+                j1 = i + 1;
+                count = 0;
+            } else {
+                j2 = i + 1;
+                count++;
             }
-            if (count == substring.length())
-                index = i;
-            count = 0;
+            if (count > higher) {
+                higher = count;
+            }
         }
-        return index;
+
+        if (count == higher) {
+            for (int i = j1; i <= j2; i++) {
+                System.out.print(string.charAt(i));
+            }
+        } else {
+            for (int i = j1 - higher - 1; i <= j2 - higher; i++) {
+                System.out.print(string.charAt(i));
+            }
+        }
     }
 }

d62bb7bd96be0a42af799b136bc175b88886de1f
Boromir99 <denispetrea2000@gmail.com>
2023-10-07 12:44:33 +0300

    Binary I/O, write 10 byte values

--- src/main/learning/Main.java
+++ src/main/learning/Main.java
@@ -1,46 +1,22 @@
 package learning;
 
 
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        java.util.Scanner input = new Scanner(System.in);
-        String string = input.next();
-        subStr(string);
+        try (FileOutputStream output = new FileOutputStream("temp.dat")) {
+            for (int i = 0; i < 10; i++)
+                output.write(i);
 
-    }
-
-    public static void subStr(String string) {
-        int j1 = 0;
-        int j2 = 0;
-        int count = 0;
-        int higher = 0;
-
-        for (int i = 0; i < string.length() - 1; i++) {
-
-            if (string.charAt(i) >= string.charAt(i + 1)) {
-                j1 = i + 1;
-                count = 0;
-            } else {
-                j2 = i + 1;
-                count++;
-            }
-            if (count > higher) {
-                higher = count;
-            }
         }
+        try (FileInputStream input = new FileInputStream("temp.dat")) {
+            int value;
+            while ((value = input.read()) != -1)
+                System.out.print(value + " ");
 
-        if (count == higher) {
-            for (int i = j1; i <= j2; i++) {
-                System.out.print(string.charAt(i));
-            }
-        } else {
-            for (int i = j1 - higher - 1; i <= j2 - higher; i++) {
-                System.out.print(string.charAt(i));
-            }
         }
     }
 }

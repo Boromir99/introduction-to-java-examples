c5d55b175fb38e81c68c9e6ede4019c18b6d40fb
Boromir99 <denispetrea2000@gmail.com>
2023-11-27 17:48:03 +0200

    Prime numbers in  a stack

--- src/main/learning/Main.java
+++ src/main/learning/Main.java
@@ -1,35 +1,29 @@
 package learning;
 
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // Create a map to store state-capital pairs
-        Map<String, String> stateCapitalMap = new HashMap<>();
-
-        // Add state-capital pairs to the map
-        stateCapitalMap.put("Romania", "Bucuresti");
-        stateCapitalMap.put("Italy", "Roma");
-        // Add more states and capitals as needed
-
-        // Prompt the user to enter a state
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a state: ");
-        String state = scanner.nextLine();
-
-        // Display the capital for the entered state
-        if (stateCapitalMap.containsKey(state)) {
-            String capital = stateCapitalMap.get(state);
-            System.out.println("The capital of " + state + " is " + capital + ".");
-        } else {
-            System.out.println("Sorry, the capital for " + state + " is not available in the map.");
+        Stack<Integer> primeNumbers = new Stack<>();
+        for (int i = 2; i < 5000; i++) {
+            int count = 0;
+            if (primeNumbers.size() < 50) {
+                for (int j = 2; j <= i / 2; j++) {
+                    if (i % j == 0) {
+                        count++;
+                        break;
+                    }
+                }
+                if (count == 0){
+                    primeNumbers.push(i);
+                }
+            } else break;
         }
+        System.out.println(primeNumbers);
     }
 }
+
 
 

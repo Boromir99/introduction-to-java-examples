10aeea99bfd690bc949a8c8409abe266e6681056
Boromir99 <denispetrea2000@gmail.com>
2024-01-04 18:28:55 +0200

    Move Zeros to end of array

--- src/main/learning/Main.java
+++ src/main/learning/Main.java
@@ -1,41 +1,30 @@
 package learning;
 
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(happyNumber(19));
+        int[] zeros = {0, 1, 0, 3, 12};
+        moveZeros(zeros);
+
+        for (int i : zeros) {
+            System.out.print(i + " ");
+        }
     }
 
-    public static int sumOfSquares(int n) {
-        int sum = 0;
-        while (n > 0) {
-            int d = n % 10;
-            sum += d;
-            n = n / 10;
-        }
-        return sum;
-    }
+    public static void moveZeros(int[] array) {
+        int zeros = 0;
 
-    public static boolean happyNumber(int n) {
-        if (n < 0) {
-            return false;
-        }
-
-        int slowSum = n;
-        int fastSum = sumOfSquares(slowSum);
-        if (slowSum == 1 && fastSum == 1) {
-            return true;
-        }
-
-        while (slowSum != fastSum) {
-            slowSum = sumOfSquares(slowSum);
-            fastSum = sumOfSquares(sumOfSquares(fastSum));
-            if (slowSum == 1 || fastSum == 1) {
-                return true;
+        for (int i = 0; i < array.length - zeros; i++) {
+            if (array[i] == 0) {
+                zeros++;
+                for (int j = i; j < array.length - zeros; j++) {
+                    array[j] = array[j + 1];
+                }
+                array[array.length - zeros] = 0;
             }
         }
-        return false;
     }
 }
 
